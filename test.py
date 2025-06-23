117e9516f4bd07ca066eca2fcc12830ade8a9cf1
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-06-22 21:52:45 -0300

    bitfieldParser, send_request and receive_piece

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ import requests
 import socket
 import struct
 
+peers = []
+
 def encode(obj):
     if isinstance(obj, str):
         return str(len(obj)).encode() + b":" + obj.encode()
@@ -81,12 +83,12 @@ def udp_tracker_announce(tracker_host, tracker_port, info_hash, peer_id, port, u
     downloaded = int(downloaded)
     left = int(left)
     uploaded = int(uploaded)
-    event = 2  # started
+    event = 2  
     
     ip = 0 
     key = random.randint(0, 0xFFFFFFFF)
-    num_want = -1  # default
-    packed_peer_id = peer_id.encode('latin1')  
+    num_want = -1  
+    packed_peer_id = peer_id.encode('ascii')  
     
     announce_packet = struct.pack(">QII20s20sQQQIIIiH", 
                                   connection_id,
@@ -129,6 +131,156 @@ def udp_tracker_announce(tracker_host, tracker_port, info_hash, peer_id, port, u
         "peers": peers
     }
 
+def handshake(peers, info_hash, peer_id):
+    valid_hanshakes = []
+    handshake_message = (
+        b'\x13' +
+        b'BitTorrent protocol' +
+        b'\x00' * 8 +
+        info_hash +                 
+        peer_id.encode('ascii')  
+    )
+
+    for ip, port_num in peers:
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(5)
+            sock.connect((ip, port_num))
+            sock.sendall(handshake_message)
+            
+            print(f"\033[1;33mHandshake sent to: {ip}:{port_num}\033[m")
+
+            response = sock.recv(68)
+
+            if len(response) != 68:
+                print(f"\033[1;31mInvalid response length from {ip}:{port_num}: {len(response)} bytes\033[m")
+                sock.close()
+                continue
+            
+
+
+            print(f"\033[1;33mReceived response: {response.hex()}\033[m")
+
+            if response[28:48] == info_hash:
+
+                print(f"\033[1;33mValid handshake with {ip}:{port_num}\033[m")
+
+                if response[0] == 19 and response[1:20] == b'BitTorrent protocol':
+
+                    print(f"\033[1;33mValid peer found: {ip}:{port_num}\033[m")
+                    valid_hanshakes.append((ip, port_num))
+                    print(f"\033[1;33mSending the interest: {ip}:{port_num}\033[m")
+
+
+                    interest_message = b'\x00\x00\x00\x01\x02'
+
+                    sock.sendall(interest_message)
+                    
+                    print(f"{"\033[1;33m"}Interest sent to: {ip}:{port_num} {"\033[m"}")
+
+                    print(f"{"\033[1;33m"}Response: {response.hex()} {"\033[m"}")
+
+
+                    while True:
+                        try:
+                            lenght_bytes = recv_all(sock, 4)
+                            lenght = int.from_bytes(lenght_bytes, byteorder='big')
+
+                            unchoke_response = recv_all(sock, lenght)
+
+                            if lenght == 0:
+                                print(f"{"\033[1;33m"} alive {"\033[m"}")
+                                continue  
+                            msg_id = unchoke_response[0]
+                            
+                            payload = unchoke_response[1:]
+        
+                            if msg_id == 1:
+                                print(f"{"\033[1;33m"}Unchoked by: {ip}:{port_num} {"\033[m"}")
+                                break
+                            elif msg_id == 0:
+                                print(f"{"\033[1;33m"}Choked by: {ip}:{port_num} {"\033[m"}")
+                                break
+                            elif msg_id == 4:
+                                print(f"{"\033[1;33m"}Have message received from: {ip}:{port_num} {"\033[m"}")
+                                break
+                            elif msg_id == 5:
+                                print(f"{"\033[1;33m"}Bitfield message received from: {ip}:{port_num} {"\033[m"}")
+                                parsed_bitfield = bitfieldParser(payload)
+                    
+                                if parsed_bitfield:
+
+                                    index_to_request = parsed_bitfield[0]
+                                    begin = 0
+                                    lenght = 2**14
+
+                                    send_request(sock, index_to_request, begin, lenght)
+                                    print(f"{"\033[1;33m"}Request sent for piece {index_to_request} from {ip}:{port_num} {"\033[m"}")
+                                else:
+                                    print(f"{"\033[1;33m"}No piece received {"\033[m"}")
+
+                                break
+                            elif msg_id == 7:
+                                print(f"{"\033[1;33m"}Piece received from: {ip}:{port_num} {"\033[m"}")
+                                break
+                            else:
+                                print(f"{"\033[1;33m"}Unknown message received from: {ip}:{port_num} {"\033[m"}")
+                                break
+
+                        except Exception as e:
+                            print(f"{"\033[1;33m"}Error receiving data from {ip}:{port_num}: {e} {"\033[m"}")
+                            break
+                    
+                       
+            sock.close()
+            
+        except socket.error as e:
+        
+            print(f"Failed to connect to {ip}:{port_num}: {e}")
+
+
+def bitfieldParser(payload):
+    pieces = []
+    for byte_index, byte in enumerate(payload):
+        for bit_index in range(8):
+            if byte & (1 << (7 - bit_index)):
+                pieces.append(byte_index * 8 + bit_index)
+    return pieces     
+  
+def send_request(sock, index, begin, length):
+    msg = struct.pack(">IBIII", 13, 6, index, begin, length)
+    sock.sendall(msg)
+
+def receive_piece(sock):
+    lenght_prefix = recv_all(sock, 4)
+    lenght = int.from_bytes(lenght_prefix, byteorder='big')
+
+    if lenght == 0:
+        print("Received keep-alive message")
+        return None
+    payload = recv_all(sock, lenght)
+    msg_id = payload[0]
+
+    if msg_id != 7:
+        raise ValueError(f"Expected message ID 7 (piece), got {msg_id}")
+        return None
+    index = int.from_bytes(payload[1:5], byteorder='big')
+    begin = int.from_bytes(payload[5:9], byteorder='big')
+    block = payload[9:]
+
+    print(f"Received piece: index={index}, begin={begin}, block_length={len(block)}")
+
+    return index, begin, block
+
+
+def recv_all(scock, lenght):
+    data = b''
+    while len(data) < lenght:
+        chuck = scock.recv(lenght - len(data))
+        if not chuck:
+            raise Exception("Connection closed before receiving all data")
+        data += chuck
+    return data
 
 def main():
     with open("Dying Light 2 [FitGirl Repack].torrent", "rb") as f:
@@ -151,7 +303,10 @@ def main():
 
     peer_id = '-ED0001-' + ''.join(random.choices('0123456789ABCDEF', k=12))
     peer_id = peer_id.ljust(20, '0')
+    
+    
     tracker_url = decoded_dict[b"announce"].decode()
+
 
     if tracker_url.startswith("udp://"):
     
@@ -172,7 +327,8 @@ def main():
             left=file_length
         )
         print("UDP Tracker response:")
-        print(response.get("peers"))
+        peers = response.get("peers")
+        handshake(peers, info_hash, peer_id)
 
     elif tracker_url.startswith("http://") or tracker_url.startswith("https://"):
         info_hash_encoded = urllib.parse.quote_from_bytes(info_hash)
